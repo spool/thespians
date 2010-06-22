5b1700667932edc17756fd67c2000718bb8f8125
Griffith Rees <griff.rees@gmail.com>
2010-06-21 22:05:12 -0600

    Clean admin.py

--- admin.py
+++ admin.py
@@ -1,13 +1,6 @@
 from django.contrib import admin
 from thespians.models import *
 
-
 admin.site.register(Role)
 admin.site.register(Person)
-
-
-class PersonAdmin(admin.ModelAdmin):
-    list_filter = ('person_types',)
-    search_fields = ('user.first_name', 'user.last_name')
-    #prepopulated_fields = {'slug': ('user.first_name','user.last_name')}
 
